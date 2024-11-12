fa0fc92aa756738240a4e21a110f03c526bfa09e
Brennon Allen <brennon.t.allen@gmail.com>
2024-11-12 11:38:39 -0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,31 @@ function App() {
     <div className="flex-center-column">
       <Picture />
       <Heading text="Brennon Allen" />
+      <br></br>
       <Subheading text="brennon.t.allen@gmail.com" />
+      <Subheading text="brennon.allen@code-tailors.com" />
       <Subheading text="+1 (726) 400-3411" />
       <div className="small-margin">
+      <Heading text="Resume" />
+        <div className="flex-center-column">
+          {/* <Link text="Cover Letter" /> */}
+          <Link text="Download" link={Resume} file={"Brennon Allen's Tech Resume"}/>
+        </div>
+        <Heading text="Portfolio" />
+        <div className="flex-center-column">
+          <Link text="Browse" link={"https://www.code-tailors.com"}/>
+        </div>
+        
         <Heading text="Links" />
-        <div className="flex-center-row">
+        <div className="flex-center-column">
           <Link text="GitHub" link={"https://github.com/Brennon-Allen"} />
           <Link text="LinkedIn" link={"https://www.linkedin.com/in/brennon-allen/"} />
           <Link text="UpWork" link={"https://www.upwork.com/freelancers/~01dc0e627900ce593c"}/>
+          <Link text="Business" link={"https://www.code-tailors.com"}/>
         </div>
       </div>
 
       <div>
-        <Heading text="Downloads" />
-        <div className="flex-center-row">
-          {/* <Link text="Cover Letter" /> */}
-          <Link text="Resume" link={Resume} file={"Brennon Allen's Tech Resume"}/>
-        </div>
       </div>
     </div>
   );
