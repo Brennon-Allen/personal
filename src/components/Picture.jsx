c70c22a77eb9dfc7209dc09a455d90526945dceb
Brennon Allen <brennon.t.allen@gmail.com>
2024-11-12 09:45:42 -0600

    fix

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,10 +1,8 @@
-import "../assets/pro-pic.png"
-
 function Picture() {
   return (
     <>
       <div className="image-box flex-center-column">
-        <img src="../assets/Pro-pic.png" alt="picture" className="image"/>
+        <img src="src/assets/Pro-pic.png" alt="picture" className="image"/>
       </div>
     </>
   );
