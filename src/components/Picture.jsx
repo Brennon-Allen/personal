663b444b7e1c269b73a0d458d814b458a61b0ab5
Brennon Allen <brennon.t.allen@gmail.com>
2024-11-12 09:42:30 -0600

    test

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -3,7 +3,7 @@ function Picture() {
   return (
     <>
       <div className="image-box flex-center-column">
-        <img src="src/assets/pro-pic.png" alt="picture" className="image"/>
+        <img src="../assets/pro-pic.png" alt="picture" className="image"/>
       </div>
     </>
   );
